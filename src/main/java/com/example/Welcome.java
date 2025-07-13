41ef12712b619bd1e8c9ac7182cd9c7b4fa4024c
Ramya86081 <2021wb86081@wilp.bits-pilani.ac.in>
2025-07-13 12:17:12 +0530

    Performing CI by modifying Welcome.java source code

--- src/main/java/com/example/Welcome.java
+++ src/main/java/com/example/Welcome.java
@@ -3,6 +3,7 @@ package com.example;
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome! I am Ramya.");
+		System.out.println("I am from WIMS 2021 batch");
 	}
 
 }
